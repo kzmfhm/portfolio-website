fb23bdb3a362405569634813b7b481f13281e163
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-23 09:14:13 -0700

    header initial setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,15 @@
+
 import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className='py-8 xl:py-12 text-white'>
+      <div className="container mx-auto">
+        <Link></Link>
+      </div>
+    </header>
   )
 }
 
