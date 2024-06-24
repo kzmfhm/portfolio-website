82784190bb0ed7f2e9d8491d0a1bfab9930c4abb
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-24 08:47:20 -0700

    refactor button

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -35,7 +35,7 @@ const Stairs = () => {
                             ease: "easeInOut",
                             delay: reverseIndex(index) * 0.1
                         }}
-                        className='h-full w-full bg-secondary relative'
+                        className='h-full w-full bg-secondary relative '
                     />
                 );
             })}
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -5,25 +5,19 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors",
   {
     variants: {
       variant: {
-        default: "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
-        destructive:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
-        outline:
-          "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
-        ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+        default: "bg-accent text-primary hover:bg-accent-hover",
+        primary:"bg-primary text-white",
+        outline: "border border-accent hover:bg-accent text-accent bg-transparent hover:text-primary"
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "px-6 h-[44px]",
+        md: "px-6 h-[48px]",
+        lg: "px-8 h-[56px] text-sm uppercase tracking-[2px]",
+       
       },
     },
     defaultVariants: {
