a9a2d38f6eca67f44fd38ae5a54429c460c1d32a
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-24 05:53:01 -0700

    complete navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -35,9 +35,20 @@ const MobileNav = () => {
             <CiMenuFries className='text-[32px] text-accent'/>
         </SheetTrigger>
         <SheetContent className="flex flex-col">
-            <div>
-                logo
+            <div className='mt-32 mb-40 text-center text-2xl'>
+            <Link href="/">
+                <h1 className='text-4xl font-semibold'>
+                kzm <span className='text-accent rounded-full'>.</span>
+                </h1>
+                </Link>
             </div>
+            <nav className='flex flex-col justify-center items-center gap-8'>
+                {links.map((link,index) => {
+                    return <Link href={link.path} key={index}
+                    className={`${link.path === pathName && "text-accent border-b-2 border-accent"} 
+                    "text-xl capitalize hover:text-accent"`}>{link.name}</Link>
+                })}
+            </nav>
         </SheetContent>
     </Sheet>
   )
