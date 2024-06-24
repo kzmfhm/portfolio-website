e1d1e3bf6cae5fbf2b7a3c267aa26212978a2ae2
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-24 00:04:07 -0700

    add navbar in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,24 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
+import Nav from './Nav'
 
 const Header = () => {
   return (
     <header className='py-8 xl:py-12 text-white'>
-      <div className="container mx-auto">
-        <Link></Link>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+        <h1 className='text-4xl font-semibold'>
+          kzm <span className='text-accent rounded-full'>.</span>
+        </h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">  <Nav/>
+        <Link href='/contact'>
+          <Button>Hire me
+          </Button>
+        </Link></div>
+      {/* Mobile nav */}
+      <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   )
---
+++ components/Nav.jsx
@@ -0,0 +1,38 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const Nav = () => {
+    const links = [
+        {name:'home',
+            path: '/'
+        },
+        {name:'services',
+            path: '/services'
+        },
+        {name:'resume',
+            path: '/resume'
+        },
+        {name:'work',
+            path: '/work'
+        },
+        {name:'contact',
+            path: '/contact'
+        },
+    ]
+
+    const pathName = usePathname();
+    console.log(pathName)
+  return (
+    <nav className='flex gap-8'>
+        {links.map((link, index) => {
+            return <Link href={link.path} key={index}
+            className={`${link.path === pathName && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
+                {link.name}</Link>
+        })}
+    </nav>
+  )
+}
+
+export default Nav
