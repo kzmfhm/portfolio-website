ecd0238c3f4b795740849141546df24ff98cefc0
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-25 07:16:50 -0700

    add image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,11 +10,11 @@ const Photo = () => {
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 0.2, ease: 'easeIn' } }}>
                 <motion.div
                     initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.2, ease: 'easeInOut' } }}
-                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute ">
-                    <Image src="/jpeg/profile.jpeg" priority alt="profile" quality={100} fill className="object-contain " />
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+                    <Image src="/jpeg/profile.jpeg" fill alt="profile" className="object-cover rounded-full" />
                 </motion.div>
                 {/* circle */}
-                {/* <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] bg-transparent"
+                <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] bg-transparent"
                 viewBox="0 0 506 506"
                 xmlns="http://www.w3.org/2000/svg">
                 <motion.circle cx="253" cy="253" r="250" 
@@ -22,7 +22,6 @@ const Photo = () => {
                     strokeWidth="4"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                  className="bg-yellow"
                     initial={{ strokeDasharray:"24 10 0 0"}}
                     animate={{strokeDasharray:["15 120 25 25", "16, 25 92 72", "4 250 22 22"],
                         rotate:[120, 360]
@@ -33,7 +32,7 @@ const Photo = () => {
                         repeatType:"reverse"
                     }}
                     />
-            </motion.svg> */}
+            </motion.svg>
             </motion.div>
         </div>
     )
