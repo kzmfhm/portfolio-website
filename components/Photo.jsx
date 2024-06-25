4d8621e399d10a7059a8f2ae98dfd8cfc5695687
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-25 07:25:56 -0700

    add photo animation

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -7,14 +7,15 @@ import Image from "next/image"
 const Photo = () => {
     return (
         <div className="w-full h-full relative">
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 0.2, ease: 'easeIn' } }}>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 0.4, ease: 'easeIn' } }}>
                 <motion.div
-                    initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.2, ease: 'easeInOut' } }}
+                    initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' } }}
                     className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
-                    <Image src="/jpeg/profile.jpeg" fill alt="profile" className="object-cover rounded-full" />
+                    <Image src="/jpeg/profile.jpeg" fill priority alt="profile" className="object-cover rounded-full" />
                 </motion.div>
                 {/* circle */}
-                <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] bg-transparent"
+                <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+                fill="transparent"
                 viewBox="0 0 506 506"
                 xmlns="http://www.w3.org/2000/svg">
                 <motion.circle cx="253" cy="253" r="250" 
@@ -23,7 +24,7 @@ const Photo = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     initial={{ strokeDasharray:"24 10 0 0"}}
-                    animate={{strokeDasharray:["15 120 25 25", "16, 25 92 72", "4 250 22 22"],
+                    animate={{strokeDasharray:["15 120 25 25", "16 25 92 72", "4 250 22 22"],
                         rotate:[120, 360]
                     }}
                     transition={{
