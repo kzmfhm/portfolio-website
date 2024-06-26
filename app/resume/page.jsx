442fe6b8c4219e9733a7b85bf424980c61ed71a9
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-26 07:32:30 -0700

    add resume content

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,63 @@
+"use client"
+import { FaReact, FaFigma, FaHtml5, FaNodeJs,FaCss3,FaJs } from "react-icons/fa"
+import {SiTailwindcss,SiNextdotJs} from "react-icons/si"
 
 
+const about = {
+    title: "About me",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sunt, molestias animi error temporibus fugiat veritatis eum! Aperiam asperiores ipsa eos distinctio quisquam eligendi ab illo error, unde tenetur laborum.",
+    info: {
+      fieldName : "Name",
+      fieldValue: "kzm"
+    },
+    info: {
+      fieldName : "Phone",
+      fieldValue: "(+92) 555-5555-55"
+    },
+    info: {
+      fieldName : "Experience",
+      fieldValue: "3+ Years"
+    },
+    info: {
+      fieldName : "Skype",
+      fieldValue: "kzm.01"
+    },
+    info: {
+      fieldName : "Email",
+      fieldValue: "johndoe@gmail.com"
+    },
+    info: {
+      fieldName : "Freelaunce",
+      fieldValue: "Available"
+    },
+    info: {
+      fieldName : "Languages",
+      fieldValue: "English Spanish"
+    }
+  }
+
+const experience = {
+  icon: "",
+  title:"My experience",
+  description: "Asperiores sunt, molestias animi error temporibus fugiat veritatis eum! Aperiam asperiores ipsa eos distinctio quisquam eligendi ab illo error, unde tenetur laborum.",
+  items : [
+    {
+      company : "Tech Solutions Inc",
+      position: "Full Stack Developer",
+      duration: "2022 - present"
+    },
+    {
+      company : "Web Design Studio",
+      position: "Front End Developer Intern",
+      duration: "Summer 2021"
+    },
+    {
+      company : "Ecommerce Startup",
+      position: "Freelaunce Web Developer",
+      duration: "Summer 2023 - winter 2024"
+    },
+  ]
+}
 const Resume = () => {
   return (
     <div></div>
