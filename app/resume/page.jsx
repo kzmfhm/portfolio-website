663141d921510891d1f1f3c12b26497968fb4440
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-26 08:37:02 -0700

    add resume data in resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -54,8 +54,80 @@ const experience = {
     {
       company : "Ecommerce Startup",
       position: "Freelaunce Web Developer",
-      duration: "Summer 2023 - winter 2024"
+      duration: "2023 - winter 2024"
     },
+    {
+      company : "Digital Agency",
+      position: "UI/UX Designer",
+      duration: "2022 - 2023"
+    },
+  ]
+}
+
+const education = {
+  icon: "",
+  title:"My education",
+  description: "Asperiores sunt, molestias animi error temporibus fugiat veritatis eum! Aperiam asperiores ipsa eos distinctio quisquam eligendi ab illo error, unde tenetur laborum.",
+  items : [
+    {
+      institution : "Online Course Plateform",
+      degree: "Full Stack Development Bootcamp",
+      duration: "2023"
+    },
+    {
+      institution : "Codecademy",
+      degree: "Frontend Track",
+      duration: "2022"
+    },
+    {
+      institution : "Online Course",
+      degree: "Programming Course",
+      duration: "2020 - 2021"
+    },
+    {
+      institution : "Virtual University of Pakistan",
+      degree: "Bs Computer Science",
+      duration: "2028 - 2022"
+    },
+  ]
+}
+
+const skills = {
+  title:"My skills",
+  description: "Asperiores sunt, molestias animi error temporibus fugiat veritatis eum! Aperiam asperiores ipsa eos distinctio quisquam eligendi ab illo error, unde tenetur laborum.",
+  skillsList : [
+    {
+      icon : <FaHtml5/>,
+      name: "html 5",
+    },
+    {
+      icon : <FaCss3/>,
+      name: "css3",
+    },
+    {
+      icon : <FaJs/>,
+      name: "javascript",
+    },
+    {
+      icon : <FaReact/>,
+      name: "react.js",
+    },
+    {
+      icon : <SiTailwindcss/>,
+      name: "tailwind.css",
+    },
+    {
+      icon : <SiNextdotJs/>,
+      name: "next.js",
+    },
+    {
+      icon : <FaNodeJs/>,
+      name: "node.js",
+    },
+    {
+      icon : <FaFigma/>,
+      name: "figma",
+    }
   ]
 }
 const Resume = () => {
