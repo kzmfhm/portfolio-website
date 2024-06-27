6fed380e4e073ebd68d2012c5712489c7a22194b
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-27 00:47:52 -0700

    add skills section in resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -220,14 +220,17 @@ const Resume = () => {
                 <h3 className="text-4xl font-bold">{skills.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
                 </div>
-                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
                   {skills.skillsList.map((skill,index) =>{
                   return <li key={index}>
-                   <TooltipProvider>
+                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger>
-                        <div className="">{skill.icon}</div>
+                      <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center items-center group">
+                        <div className="text-6xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
                       </TooltipTrigger>
+                      <TooltipContent>
+                        <p className="capitalize">{skill.name}</p>
+                      </TooltipContent>
                     </Tooltip>
                    </TooltipProvider>
                   </li>
