5e68e0dfd38d1972a75e872d7829878876f09228
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-27 02:04:31 -0700

    starting work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,21 @@
+"use client"
+
+import { motion } from "framer-motion"
+import React, { useState } from "react"
+import {SwiperSlide,Swiper } from "swiper/react"
+import "swiper/css"
+import { BsArrowUpRight, BsGithub } from "react-icons/bs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import Link from "next/link"
+import Image from "next/image"
 
 
+const projects = [
+  {
+    num: "01",
+    category : "frontend"
+  }
+]
 const Work = () => {
   return (
     <div></div>
