b552f0715ac1f6a76ded6566b55725309548d970
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-27 00:59:41 -0700

    add social path

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -3,10 +3,10 @@ import {FaGithub,FaLinkedinIn,FaFigma, FaTwitter} from "react-icons/fa"
 
 
 const socials = [
-  {icons: <FaGithub/>, path:''},
-  {icons: <FaLinkedinIn/>, path:''},
+  {icons: <FaGithub/>, path:'https://github.com/kzmfhm/portfolio-website'},
+  {icons: <FaLinkedinIn/>, path:'https://www.linkedin.com/in/khuzaima-n-658b98268/'},
   {icons: <FaTwitter/>, path:''},
-  {icons: <FaFigma/>, path:''}
+  {icons: <FaFigma/>, path:'https://www.figma.com/files/team/1304670062254728131/recents-and-sharing/recently-viewed?fuid=1241374001239227732'}
 ]
 const Socials = ({iconStyles,containerStyles}) => {
   
