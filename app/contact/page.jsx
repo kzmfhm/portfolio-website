e710e50db343e222c42fbdf65527e50bb91dc656
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-28 09:10:59 -0700

    start contact page code

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Select, SelectItem, SelectContent, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
 
 
 const Contact = () => {
