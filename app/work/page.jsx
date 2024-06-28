c4e7733404f22b2a2dc32a34b888b0e75c430aad
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-28 08:12:50 -0700

    add right and left icons

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 import "swiper/css";
@@ -8,11 +7,34 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import Link from "next/link";
 import Image from "next/image";
 import { SwiperSlide, Swiper } from "swiper/react";
+import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
 
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="p-3 bg-slate-100 hover:text-white/60 hover:bg-accent cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute left-2 top-1/2"
+      onClick={onClick}
+    >
+      <PiCaretLeftLight className="text-primary text-lg"/>
+    </div>
+  );
+};
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="p-3 bg-slate-100 hover:text-white/60 hover:bg-accent cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute right-2 top-1/2"
+      onClick={onClick}
+    >
+      <PiCaretRightLight className="text-primary text-lg"/>
+    </div>
+  );
+};
 const projects = [
   {
     num: "01",
-    category: "frontend",
+
     title: "food website",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -26,7 +48,7 @@ const projects = [
   },
   {
     num: "02",
-    category: "frontend",
+
     title: "3D form",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -40,7 +62,7 @@ const projects = [
   },
   {
     num: "03",
-    category: "desktop application",
+
     title: "image converter",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -53,7 +75,7 @@ const projects = [
   },
   {
     num: "04",
-    category: "desktop application",
+ 
     title: "email scraper",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -67,7 +89,7 @@ const projects = [
   },
   {
     num: "05",
-    category: "frontend",
+
     title: "tic-tac-toe game",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -81,7 +103,7 @@ const projects = [
   },
   {
     num: "06",
-    category: "fullstack",
+  
     title: "personal-portfolio",
     description: "Laborum quos sit quisquam quae similique laboriosam necessitatibus aliquam quam voluptatem at ullam voluptates minus, cum, in qui!",
     stack: [
@@ -113,13 +135,15 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
+      
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               {/* outline num */}
               <div className="text-6xl font-bold text-transparent text-outline">{project.num}</div>
-              {/* category */}
-              <h2 className="text-[32px] font-bold text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+              {/* title*/}
+         
+              <h2 className="text-[32px] font-bold text-white group-hover:text-accent transition-all duration-500 capitalize">{project.title}</h2>
               {/* description */}
               <p className="text-white/60">{project.description}</p>
               {/* stack */}
@@ -176,9 +200,11 @@ const Work = () => {
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
+                      <PrevArrow/>
                     <div className="relative group h-[460px] flex justify-center items-center bg-pink-50/20">
-                      {/* overlay */}
-                      <div className=""></div>
+                    
+                     
+                    
                       {/* image */}
                       <div className="relative w-full h-full">
                         <Image
@@ -189,13 +215,16 @@ const Work = () => {
                           unoptimized={project.image.endsWith(".gif")} 
                         />
                       </div>
-                    </div>
+                  
+                      </div>
+                      <NextArrow/>
                   </SwiperSlide>
                 );
               })}
             </Swiper>
           </div>
         </div>
+    
       </div>
     </motion.section>
   );
