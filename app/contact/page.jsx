6bcd9b227f0d01fca46c31270fdc63c6b36c36df
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-28 22:58:34 -0700

    fix input styling on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -39,7 +39,15 @@ const Contact = () => {
         {/* form */}
         <div className="xl:h-[54%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-            <h3>Let&#39;s work together</h3>
+            <h3 className="text-3xl text-accent">Let&#39;s work together</h3>
+            <p className="text-white/60">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Exercitationem ab iure architecto eaque.</p>
+          {/* input */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <Input type="firstname" placeholder="First Name"/>
+            <Input type="lastname" placeholder="Last Name"/>
+            <Input type="email" placeholder="Email Address"/>
+            <Input type="phone" placeholder="Phone Number"/>
+          </div>
           </form>
         </div>
         {/* info */}
