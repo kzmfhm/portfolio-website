3fddaef5cca8e47437b815ddf90b82aec4e53302
kzm/fhm <khuzaimanazik4@gmail.com>
2024-06-29 07:24:39 -0700

    fix input styling on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
      <div className="mx-auto container">
       <div className="flex flex-col xl:flex-row gap-[30px]">
         {/* form */}
-        <div className="xl:h-[54%] order-2 xl:order-none">
+        <div className="xl:w-[54%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
             <h3 className="text-3xl text-accent">Let&#39;s work together</h3>
             <p className="text-white/60">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Exercitationem ab iure architecto eaque.</p>
@@ -48,11 +48,44 @@ const Contact = () => {
             <Input type="email" placeholder="Email Address"/>
             <Input type="phone" placeholder="Phone Number"/>
           </div>
-          </form>
+          {/* select */}
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a service"/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+              <SelectLabel className="">
+                Select a service
+              </SelectLabel>
+              <SelectItem value="est">Web Development</SelectItem>
+              <SelectItem value="cst">UI/UX Design</SelectItem>
+              <SelectItem value="mst">Logo Design</SelectItem>
+              </SelectGroup >
+            </SelectContent>
+          </Select>
+          <Textarea className="h-[200px]" placeholder="Type your message here"/>
+          {/* buttons */}
+          <Button size="md" className="xl:w-[200px] px-4">Send message</Button>
+        </form>
         </div>
         {/* info */}
         <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-          info
+       <ul className="flex flex-col gap-10">
+        {info.map((item,index) => {
+          return <li key={index} className="flex items-center gap-6">
+            <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+              <div className="text-[28px]">
+                {item.icon}
+              </div>
+              </div>
+              <div className="flex-1">
+                <p className="text-white/60">{item.title}</p>
+                <h3 className="text-lg">{item.description}</h3>
+              </div>
+          </li>
+        })}
+       </ul>
         </div>
       </div>
      </div>
